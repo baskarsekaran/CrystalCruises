08899bb1463e1db9bc8bc202c66a02ecf299b820
baskarsekaran <baskar10576@gmail.com>
2014-08-08 13:12:57 -0700

    updated
    updated

--- CrystalCrusiesLeopardTeam/src/crystalcrusiestestng/CallingClassName.java
+++ CrystalCrusiesLeopardTeam/src/crystalcrusiestestng/CallingClassName.java
@@ -12,13 +12,13 @@ public class CallingClassName {
 		String className = this.getClass().getSimpleName();
 		System.out.println(className);
 		// TODO Auto-generated method stub
-		System.out.println("Edited in GIT Hub");
 		StringWriter sw = new StringWriter();  
 		PrintWriter pw = new PrintWriter(sw);  
 		new Throwable().printStackTrace(pw);  
 		String stackTrace = sw.toString(); 
 		System.out.println(stackTrace);
 		return stackTrace;
+		System.out.println("Edited in GIT Hub");
 
 	}
 
